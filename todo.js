390464ecd0c999a1cffa674c93952ce9f0e0ade2
Ahmed Abdalla <dynamic.bineuro@gmail.com>
2022-08-21 23:38:16 -0700

    Added delete button to my to do

--- todo.js
+++ todo.js
@@ -1,4 +1,5 @@
 const list = [];
+let counter = 0;
 //class responsible for representing a single entry in the todo list
 class todoEntry {
     entry;
@@ -25,7 +26,8 @@ function addEntry() {
         document.getElementById("list").innerHTML +=
             "<li class=\"listItem\" id=\"item\"\">" + list[lastIndex].entry +
             "<input type=\"checkbox\" class=\"Check\" value=\"no\" id=\"" +
-            lastIndex + "\" onclick=\"checkEntry(this.id)\"></li>";
+            lastIndex + "\" onclick=\"checkEntry(this.id)\"> <button class=\"Delete\" id=\""
+            + lastIndex + "\" onclick=\"removeEntry(this.id)\">X</button></li>";
     }
     else {
         alert("Please enter a valid entry");
@@ -41,7 +43,8 @@ function loadEntry(entryText) {
         document.getElementById("list").innerHTML +=
             "<li class=\"listItem\" id=\"item\"\">" + list[lastIndex].entry +
             "<input type=\"checkbox\" class=\"Check\" value=\"no\" id=\"" +
-            lastIndex + "\" onclick=\"checkEntry(this.id)\"></li>";
+            lastIndex + "\" onclick=\"checkEntry(this.id)\"> <button class=\"Delete\" id=\""
+            + lastIndex + "\" onclick=\"removeEntry(this.id)\">X</button></li>";
     }
 }
 //checks entry at specific index
@@ -54,10 +57,13 @@ function update() {
     for (i = 0; i < list.length; i++) {
         if (list[i].isChecked()) {
             list.splice(i, 1);
+            counter++;
             i -= 1;
         }
+        localStorage.setItem("counter", counter);
     }
     localStorage.setItem("list", JSON.stringify(list));
+    updateCounter();
     reconstructList()
 }
 function reconstructList() {
@@ -67,14 +73,23 @@ function reconstructList() {
             "<li class=\"listItem\" id=\"item\"\">" +
             list[i].entry +
             "<input type=\"checkbox\" class=\"Check\" id=\""
-            + i + "\" onclick=\"checkEntry(this.id)\"></li>";
+            + i + "\" onclick=\"checkEntry(this.id)\">" +
+            "<button class=\"Delete\" id=\""
+            + i + "\" onclick=\"removeEntry(this.id)\">X</button></li>";
     }
+}
+function removeEntry(index) {
+    if (index < list.length && index >= 0)
+        list.splice(index, 1);
+    reconstructList();
 }
 function clearLocal() {
     localStorage.clear();
     list.splice(0, list.length);
+    counter = 0;
     console.log(list);
     reconstructList();
+    updateCounter();
 }
 function setupList() {
     if (localStorage.getItem("list") === null) {
@@ -85,5 +100,20 @@ function setupList() {
         for (i = 0; i < entries.length; i++) {
             loadEntry(entries[i].entry);
         }
+    }
+    if (localStorage.getItem("counter") === null) {
+        return;
+    }
+    else {
+        counter = localStorage.getItem("counter");
+    }
+}
+function updateCounter() {
+    if (counter > 0) {
+        document.getElementById("taskCounter").innerHTML =
+            "You have finished " + counter + " tasks!";
+    }
+    else {
+        document.getElementById("taskCounter").innerHTML = "";
     }
 }
